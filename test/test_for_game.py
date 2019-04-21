022a88cf25f11cac63d25e30c38544867342d79d
WeijieChen2017 <weijiechen1994@gmail.com>
2019-04-21 00:22:37 -0500

    ready for simulation

--- test/test_for_game.py
+++ test/test_for_game.py
@@ -7,9 +7,13 @@ from policy.pure_policy import *
 
 
 # basic setting
-bank_set_value("n_player", 4)
-bank_set_value("n_coin", 5)
-bank_set_value("n_max_game", 5)
+n_player = 4
+n_coin = 5
+n_max_game = 5
+
+bank_set_value("n_player", n_player)
+bank_set_value("n_coin", n_coin)
+bank_set_value("n_max_game", n_max_game)
 
 chessboard = A2F_Chessboard([2, 3, 5, 3, 2])
 player_0 = A2F_Player([1, 0, 0, 0])
@@ -22,14 +26,25 @@ coin_0 = A2F_Coin([0, 0, 0, 0])
 game = A2F_Game()
 coin_list = []
 
-for idx_round in range(bank_get_value("n_max_game")):
+for idx_round in range(n_max_game):
     action_list = []
-    for idx_player in range(bank_get_value("n_player")):
+
+    # n-1 random player
+    for idx_player in range(n_player-1):
         random_player = A2FP_Random(game=game,
                                     chessboard=chessboard,
                                     player=player_list[idx_player])
         random_player.predict()
         action_list.append(random_player.decide())
+
+    # 1 greedy player
+    greedy_player = A2FP_Greedy(game=game,
+                                chessboard=chessboard,
+                                player=player_list[n_player-1])
+    greedy_player.predict()
+    action_list.append(greedy_player.decide())
+
+    # simulate result
     curr_action = A2F_Action(action_list)
     curr_round = A2F_Round(chessboard=chessboard, action=curr_action)
     coin_list.append(curr_round.run())
